cf45c8f7d57b9d66f5237cc574171497a91c3c50
ghf <guohf@instrument.com.cn>
2019-04-17 15:16:26 +0800

    MyBatisPlus反向工程

---
+++ src/main/java/com/instrument/mybatisplusgenerator/MybatisplusgeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.instrument.mybatisplusgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisplusgeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisplusgeneratorApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/instrument/mybatisplusgenerator/SqlServiceGenerator.java
@@ -0,0 +1,95 @@
+package com.instrument.mybatisplusgenerator;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <p>
+ * SQL Service代码生成器演示例子
+ * </p>
+ *
+ * @author jobob
+ * @since 2018-09-12
+ */
+public class SqlServiceGenerator {
+
+    /**
+     * RUN THIS
+     */
+    public static void main(String[] args) {
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir");
+        gc.setOutputDir(projectPath + "/src/main/java");
+        //指定代码生成人
+        gc.setAuthor("ghf");
+        //生成完毕后，是否默认打开生成目录
+        gc.setOpen(false);
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:sqlserver://172.16.8.202;DatabaseName=instrument");
+        // dsc.setSchemaName("public");
+        dsc.setDriverName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        dsc.setUsername("testdb");
+        dsc.setPassword("Ins@123");
+        mpg.setDataSource(dsc);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        //指定模块名称
+        pc.setModuleName("generator");
+        //指定模块路径
+        pc.setParent("com.instrument.mybatisplusgenerator");
+        mpg.setPackageInfo(pc);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+        List<FileOutConfig> focList = new ArrayList<>();
+        focList.add(new FileOutConfig("/templates/mapper.xml.ftl") {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输入文件名称
+                return projectPath + "/src/main/resources/mapper/" + pc.getModuleName()
+                        + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+            }
+        });
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+        mpg.setTemplate(new TemplateConfig().setXml(null));
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        //strategy.setSuperEntityClass("com.baomidou.mybatisplus.samples.generator.common.BaseEntity");
+        strategy.setEntityLombokModel(true);
+       // strategy.setSuperControllerClass("com.baomidou.mybatisplus.samples.generator.common.BaseController");
+        //需要生成的表
+        strategy.setInclude(new String[]{"VIP","tt"});
+        strategy.setSuperEntityColumns("id");
+        strategy.setControllerMappingHyphenStyle(true);
+        strategy.setTablePrefix(pc.getModuleName() + "_");
+        mpg.setStrategy(strategy);
+        // 选择 freemarker 引擎需要指定如下加，注意 pom 依赖必须有！
+        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        mpg.execute();
+    }
+
+}
